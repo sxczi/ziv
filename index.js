160ad890ade984ea43c266d2b697072fbeaec7c2
uwuxia <uwuxia@uwuxias-MacBook-Air.local>
2022-02-01 15:30:33 +0300

    aaaaa

--- index.js
+++ index.js
@@ -57,72 +57,99 @@ if (fs.existsSync(`${args[0]}.ziv`)) {
 } else {
   console.log(
     chalk.bgRed(
-      `Error: ${path.resolve(args[0] + ".ziv")}: This file doesn't exist`
+      `Error: ${
+        args[0]
+          ? path.resolve(args[0] + ".ziv: This file doesn't exist")
+          : "Please provide a file"
+      }`
     )
   );
 }
 
 function parse(code) {
-  for (let i = 0; i < code.length; i++) {
-    let line = code[i];
-    if (!line.startsWith("#")) {
-      const currentLine = line.trim().replace(/\s+/g, " ").split(" ");
+  try {
+    for (let i = 0; i < code.length; i++) {
+      let line = code[i];
+      let lineNumber = i + 1;
+      if (!line.startsWith("#")) {
+        const currentLine = line.trim().replace(/\s+/g, " ").split(" ");
 
-      for (let j = 0; j < currentLine.length; j++) {
-        let word = currentLine[j];
+        for (let j = 0; j < currentLine.length; j++) {
+          let word = currentLine[j];
 
-        if (word === "mutable" || word === "immutable") {
-          let val = line.replace(/\s+/g, " ").split(" ").slice(3).join(" ");
+          if (word === "mutable" || word === "immutable") {
+            let val = line.replace(/\s+/g, " ").split(" ").slice(3).join(" ");
 
-          const evaluate = (str) => {
-            if (/^[0-9()+\-*.\/]*$/.test(str)) {
-              return eval(str);
-            } else {
-              let string = line
-                .replace(/\s+/g, " ") // removes extra whitespace between words
-                .split(" ")
-                .slice(3)
-                .join(" ");
-              if (string.split(" ")[0] === "input") {
-                return `"${inp(eval(string.split(" ").slice(1).join(" ")))}"`;
-              } else return str;
-            }
-          };
+            const evaluate = (str) => {
+              if (/^[0-9()+\-*.\/]*$/.test(str)) {
+                return eval(str);
+              } else {
+                let string = line
+                  .replace(/\s+/g, " ") // removes extra whitespace between words
+                  .split(" ")
+                  .slice(3)
+                  .join(" ");
+                if (string.split(" ")[0] === "input") {
+                  return `"${inp(eval(string.split(" ").slice(1).join(" ")))}"`;
+                } else return str;
+              }
+            };
 
-          val = evaluate(val);
+            val = evaluate(val);
 
-          callstack.push({
-            type: "variable",
-            kind: word,
-            identifier: currentLine[currentLine.indexOf(word) + 1],
-            value: val,
-            implicitType: datatypes.filter((value) => value.regex.test(val))[0],
-          });
-        } else if (word === "output") {
-          console.log(
-            eval(
-              line
-                .replace(/\s+/g, " ")
-                .split(" ")
-                .slice(1)
-                .join(" ")
-                .replace(/(?<!\w)@\w+/g, (x) => {
-                  // for words that start with @
-                  const variable = callstack.items.filter((item) => {
-                    if (
-                      item.type === "variable" &&
-                      item.identifier === x.split("@")[1]
-                    ) {
-                      return item.value;
-                    }
-                  });
-                  // console.log(eval(variable[0].value))
-                  return eval(variable[0]?.value || null);
-                })
-            )
-          );
+            callstack.push({
+              type: "variable",
+              kind: word,
+              identifier: currentLine[currentLine.indexOf(word) + 1],
+              value: val,
+              implicitType: datatypes.filter((value) =>
+                value.regex.test(val)
+              )[0],
+            });
+            break;
+          } else if (word === "output") {
+            console.log(
+              eval(
+                line
+                  .replace(/\s+/g, " ")
+                  .split(" ")
+                  .slice(1)
+                  .join(" ")
+                  .replace(/(?<!\w)@\w+/g, (x) => {
+                    // for words that start with @
+                    const variable = callstack.items.filter((item) => {
+                      if (
+                        item.type === "variable" &&
+                        item.identifier === x.split("@")[1]
+                      ) {
+                        return item.value;
+                      }
+                    });
+                    // console.log(eval(variable[0].value))
+                    return eval(variable[0]?.value || null);
+                  })
+              )
+            );
+            break;
+          } else if (callstack.items.some((item) => item.identifier === word)) {
+            let val = line.replace(/\s+/g, " ").split(" ").slice(2).join(" ");
+
+            callstack.items.filter((item) => {
+              if (item.identifier === word) {
+                if (item.kind === "mutable") {
+                  item.value = val;
+                } else {
+                  throw `Error: ${path.resolve(
+                    args[0] + ".ziv"
+                  )}:${lineNumber}\n"${line}" -> You can't reassign an immutable variable!`;
+                }
+              }
+            });
+          }
         }
       }
     }
+  } catch (err) {
+    console.log(chalk.bgRed(err));
   }
 }
